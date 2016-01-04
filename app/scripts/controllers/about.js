cab5502ebd3aca6963f733d6c26341538d38f3b2
francoisa <andre.francois@gmail.com>
2016-01-03 19:54:30 -0500

    Yoeman based first version of project.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cookbookApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cookbookApp
+ */
+angular.module('cookbookApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cookbookApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cookbookApp
+ */
+angular.module('cookbookApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
